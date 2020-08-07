d85ced77cbb0f6d1664b811659129bd61304b3a2
Alan Lima <alima@outlook.com>
2020-08-07 16:23:58 +1000

    add efs module

---
+++ src/terraform/modules/efs/variables.tf
@@ -0,0 +1,26 @@
+variable "project" {
+  description = "define the class id from devops academy"
+  type        = string
+}
+
+variable "common_tags" {
+  description = "define tags which will be applied for every resource created"
+  type        = map(string)
+  default     = {}
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "ecs_sg_id" {
+    type = string
+}
